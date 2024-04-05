442994e95e11eb915ca5ed558d404d301652cfde
Bradlee <69256931+StupidRepo@users.noreply.github.com>
2024-04-06 00:25:58 +0100

    goodbye remappedSrc

--- remappedSrc/com/stupidrepo/modifiable_damage/client/ModifiableDamageClient.java
+++
@@ -1,9 +0,0 @@
-package com.stupidrepo.modifiable_damage.client;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class ModifiableDamageClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-    }
-}
